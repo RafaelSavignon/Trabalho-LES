7eb558172cc98931c39034ed43621d4d4678f143
Rafael Savignon de Resende <105799578+RafaelSavignon@users.noreply.github.com>
2024-05-02 04:39:53 -0300

    Add files via upload

---
+++ src/lojaroupas/controller/TelaLoginController.java
@@ -0,0 +1,17 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class TelaLoginController {
+
+    @FXML
+    private Button BtnLogin;
+
+    @FXML
+    private TextField TextFieldLogin;
+
+    @FXML
+    private PasswordField TextFieldSenha;
+
+}
